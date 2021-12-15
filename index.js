4d5df1d58714d0893c7e53af761ef66346a124dc
venomiele <atrandafir35@yahoo.com>
2021-12-15 07:23:59 -0800

    Added index.js library itself

---
+++ index.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", () => {
+    var typed = new Typed('.animate', {
+        strings: ["Cats", 
+        "Kittens"],
+        typeSpeed: 100,
+        backDelay: 900,
+        backSpeed: 100,
+        cursorChar: "|",
+        startDelay: 2000,
+        smartBackspace: true,
+      });
+})
